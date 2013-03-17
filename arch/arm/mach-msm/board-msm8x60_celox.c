be2fe94637a20695b2c6066f54b47ec04ab6253f
elbermu <angbermu@gmail.com>
2013-03-16 23:17:22 -0400

    gov: remove hardcoded values for 2nd-phase, can now be modified in defconfig

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -17169,7 +17169,7 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-        set_two_phase_freq(1242000);
+        set_two_phase_freq(CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE_FREQ);
 #endif
 
 	/*
